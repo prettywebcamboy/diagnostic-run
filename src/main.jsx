25085ee750f76eb82f6b4a419bde802d5ea0e95f
prettyboy <96651293+prettywebcamboy@users.noreply.github.com>
2026-09-04 03:20:01 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
-
 function App() {
-  return <div className="message">https://discord.gg/CNVAJuMqDh</div>;
+  return (
+    <a
+      className="message"
+      href="https://discord.gg/CNVAJuMqDh"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      click me
+    </a>
+  );
 }
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
